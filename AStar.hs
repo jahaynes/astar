c06603aab3a9128805f4f4da5846d53eda6feaae
John Haynes <john.andrew.haynes@gmail.com>
2019-01-01 19:14:37 +0100

    finish the algorithm

--- AStar.hs
+++ AStar.hs
@@ -1,10 +1,10 @@
 import           Data.Heap   (Heap)
-import qualified Data.Heap            as H
+import qualified Data.Heap                 as H
 import           Data.Maybe  (mapMaybe)
 import           Data.Set    (Set)
-import qualified Data.Set             as S
+import qualified Data.Set                  as S
 import           Data.Vector (Vector, (!))
-import qualified Data.Vector          as V
+import qualified Data.Vector               as V
 
 type Grid a = Vector (Vector a)
 
@@ -18,9 +18,6 @@ globalGrid = V.fromList . map V.fromList
        , "    * s   *"
        , "    *******"
        ]
-
-dist :: (Int, Int) -> (Int, Int) -> Int
-dist (a,b) (c,d) = abs (a-c) + abs (b-d)
 
 find :: Eq a => a -> Grid a -> Maybe (Int, Int)
 find item = goRow 0
@@ -40,57 +37,65 @@ find item = goRow 0
 data Step = Step { getPathCost      :: !Int
                  , getHeuristicCost :: !Int
                  , getCoord         :: !(Int, Int)
-                 } deriving Show
+                 , getPredecessor   :: !(Maybe Step)
+                 } deriving (Eq, Show)
 
-instance Eq Step where
-    Step _ _ c1 == Step _ _ c2 = c1 == c2
-                 
 instance Ord Step where
-    Step p1 h1 c1 <= Step p2 h2 c2 = (p1+h1, c1) <= (p2+h2, c2)
+    Step p1 h1 c1 pr1 <= Step p2 h2 c2 pr2 = (p1+h1, c1, pr1) <= (p2+h2, c2, pr2)
 
 main :: IO ()
 main = do
     let (Just start) = find 's' globalGrid
         (Just goal)  = find 'g' globalGrid
-    print (start, goal)
-    print =<< astar globalGrid start goal
-    return ()
+    print $ astar globalGrid start goal
 
-astar :: Grid Char -> (Int, Int) -> (Int, Int) -> IO Bool
+astar :: Grid Char -> (Int, Int) -> (Int, Int) -> Maybe [(Int, Int)]
 astar grid start goal = do
 
     let step0 = Step { getPathCost      = 0
                      , getHeuristicCost = dist start goal
                      , getCoord         = start
+                     , getPredecessor   = Nothing
                      }
 
-    go (H.singleton step0) S.empty
+    extractPath [] <$> findPath (H.singleton step0) S.empty
 
     where
-    go :: Heap Step -> Set (Int, Int) -> IO Bool
-    go fringe done =
-        case H.uncons fringe of
-            Nothing -> return False
-            Just (Step pc _ c@(i,j), fs) ->
-                if c == goal
-                    then return True
-                    else do
-                        print (pc, c)
-                        let done' = S.insert c done
-                            nextCoords = mapMaybe (check grid done) [ (i+1, j  )
-                                                                    , (i  , j+1)
-                                                                    , (i-1, j  )
-                                                                    , (i  , j-1)
-                                                                    ]
-                            nextSteps = H.fromList . map (\nc -> Step (pc+1) (dist nc goal) nc) $ nextCoords
-                        go (H.union fs nextSteps) done'
+    extractPath :: [(Int, Int)] -> Step -> [(Int, Int)]
+    extractPath acc step =
+        let acc' = getCoord step : acc
+        in case getPredecessor step of
+              Just step' -> extractPath acc' step'
+              Nothing    -> acc'
 
-check :: Grid Char -> Set (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
-check grid done (i,j)
-    | i <  0                   = Nothing
-    | i >= V.length grid       = Nothing
-    | j <  0                   = Nothing
-    | j >= V.length (grid ! i) = Nothing
-    | (i,j) `S.member` done    = Nothing
-    | grid ! i ! j == '*'      = Nothing
-    | otherwise                = Just (i, j)
+    findPath :: Heap Step -> Set (Int, Int) -> Maybe Step
+    findPath fringe done = do
+
+        (s@(Step pc _ c@(i,j) _), fs) <- H.uncons fringe
+
+        if c == goal
+            then Just s
+            else do
+                let nextCoords = mapMaybe (check done) [ (i+1, j  )
+                                                       , (i  , j+1)
+                                                       , (i-1, j  )
+                                                       , (i  , j-1)
+                                                       ]
+                    nextSteps = H.fromList
+                              . map (\nc -> Step (pc+1) (dist nc goal) nc (Just s))
+                              $ nextCoords
+
+                findPath (H.union fs nextSteps) (S.insert c done)
+
+    dist :: (Int, Int) -> (Int, Int) -> Int
+    dist (a,b) (c,d) = abs (a-c) + abs (b-d)
+
+    check :: Set (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+    check done (i,j)
+        | i <  0                   = Nothing
+        | i >= V.length grid       = Nothing
+        | j <  0                   = Nothing
+        | j >= V.length (grid ! i) = Nothing
+        | (i,j) `S.member` done    = Nothing
+        | grid ! i ! j == '*'      = Nothing
+        | otherwise                = Just (i, j)
